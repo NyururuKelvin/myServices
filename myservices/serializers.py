05731bfad1f30498fc2ca695bfbaa309d035e358
Nyururukelvin <nyururukelvin9@gmail.com>
2020-11-04 11:10:16 +0300

    my commit add methods & extra kwargs

--- myservices/serializers.py
+++ myservices/serializers.py
@@ -5,4 +5,9 @@ from rest_framework import serializers
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = User
-        fields = ['url', 'username', 'email', 'groups']
+        fields = ('id', 'username', 'email', 'password')
+        extra_kwargs = {'password' :{'write_only':True, 'required':True}}
+
+    def create(self, validated_data):
+        user = User.objects.create_user(**validated_data)
+        return user
