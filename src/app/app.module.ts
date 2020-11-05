5edde668d68cff244e44b8ba6de4fa7a2447fdd5
AmimoG <gilbertmatete6@gmail.com>
2020-11-05 15:21:03 +0300

    business cards added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BusinessComponent } from './business/business.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BusinessComponent
   ],
   imports: [
     BrowserModule
