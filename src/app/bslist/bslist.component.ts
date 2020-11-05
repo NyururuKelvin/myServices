34a43b65915648e5db641792a305a3134cc00c3d
AmimoG <gilbertmatete6@gmail.com>
2020-11-05 17:49:58 +0300

    bslist component added

---
+++ src/app/bslist/bslist.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-bslist',
+  templateUrl: './bslist.component.html',
+  styleUrls: ['./bslist.component.css']
+})
+export class BslistComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
